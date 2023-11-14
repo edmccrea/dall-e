4e7749c93726f6d641013ddf12b20beb535b9330
edmccrea <ed-mccrea@hotmail.co.uk>
2023-11-14 22:19:10 +0100

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
